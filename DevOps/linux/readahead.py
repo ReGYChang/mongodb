35b60f35f0cacb6eb139169dcfa645f6ca599d62
regy <p714140432@gmail.com>
2021-08-04 14:00:17 +0800

    feat<readahead>: setup readahead value

---
+++ DevOps/linux/readahead.py
@@ -0,0 +1,22 @@
+from utils import *
+import os
+import re
+
+def setup_readahead():
+
+     # ensure tuned.conf path exists
+    if(os.path.isfile("/lib/tuned/throughput-performance/tuned.conf") == False):
+        print("Fatal Error: tuned.conf file does not exist, please checkout.")
+        exit()
+    else:
+        with open('/lib/tuned/throughput-performance/tuned.conf', 'r') as tunedconf_file :
+            tunedconf = tunedconf_file.read()
+        print("Message: tuned.conf file check completed successfully.")
+
+    with open('/etc/sysctl.conf', 'w') as tunedconf_file:
+        sysconf = re.sub(r"readahead=>.+","readahead=>16",tunedconf)
+        tunedconf_file.write(sysconf)
+
+    read_process("service tuned restart")
+
+    print(read_process("blockdev --report"))
