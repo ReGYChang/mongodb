f23b3b25a4890097373d044d2a74af19a2a46371
regy <p714140432@gmail.com>
2021-08-05 15:33:07 +0800

    feat<health_check>: add get_colls_indexes func

---
+++ DevOps/health_check_script/get_colls_indexes.js
@@ -0,0 +1,16 @@
+// get all colls stats //
+// db.getMongo().setSlaveOk()
+rs.secondaryOk();
+
+var alldbs = db.getMongo().getDBNames();
+
+for(var j = 0; j < alldbs.length; j++){
+    var db = db.getSiblingDB(alldbs[j]);
+
+    print("\n\n================================== DB: " + db.getName() + " ==================================")
+
+    db.getCollectionNames().forEach((c) => {
+        print("\n\nCollection: " + c)
+        printjson(db[c].getIndexes());
+    });
+}
