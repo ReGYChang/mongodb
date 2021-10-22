359a1c30b2b4f939fbedc094e59c87409cdeec3d
ReGYChang <p714140432@gmail.com>
2021-10-22 15:11:03 +0800

    fix<parsing>: add fields dumplicated issue

--- DevOps/health_check_analysis/addFields.js
+++ DevOps/health_check_analysis/addFields.js
@@ -2,15 +2,15 @@ var db = db.getSiblingDB(company);
 var ts = (new Date()).getTime();
 
 // add host to coll_stats docs
-db.coll_stats.update({ $and: [{ host: { $not: { host: hostname } } }, { operationTime: { $gte: Timestamp(health_check_start, 1), $lt: Timestamp(health_check_end, 1) } }] }, { $set: { host: hostname, ts: ts } }, { multi: true });
+db.coll_stats.update({ $and: [{ host: { $exists: false } }, { operationTime: { $gte: Timestamp(health_check_start, 1), $lt: Timestamp(health_check_end, 1) } }] }, { $set: { host: hostname, ts: ts } }, { multi: true });
 
 // add host to db_stats docs
-db.db_stats.update({ $and: [{ host: { $not: { host: hostname } } }, { operationTime: { $gte: Timestamp(health_check_start, 1), $lt: Timestamp(health_check_end, 1) } }] }, { $set: { host: hostname, ts: ts } }, { multi: true });
+db.db_stats.update({ $and: [{ host: { $exists: false } }, { operationTime: { $gte: Timestamp(health_check_start, 1), $lt: Timestamp(health_check_end, 1) } }] }, { $set: { host: hostname, ts: ts } }, { multi: true });
 
 //add frag info to coll_stats
 {
     mongodb_rs_frag.forEach(fragInfo => {
         ns = fragInfo.db + '.' + fragInfo.collection;
-        db.coll_stats.update({ ns: ns }, { $set: { fragInfo: fragInfo } }, { multi: true });
+        db.coll_stats.update({ $and: [{ fragInfo: { $exists: false } }, { ns: ns }, { host: hostname }] }, { $set: { fragInfo: fragInfo } }, { multi: true });
     });
 }
