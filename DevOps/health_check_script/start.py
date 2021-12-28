8f4ddb401abf28021f244a9183852a649888c6c9
Regy <regy@iro.local>
2021-12-28 16:43:38 +0800

    fix<healthCheck>: progress bar not found

--- DevOps/health_check_script/start.py
+++ DevOps/health_check_script/start.py
@@ -8,5 +8,3 @@ for f in os.listdir(path):
     if f.endswith('txt'):
         print(f)
         os.rename(f, os.path.splitext(f)[0] +'.js')
-
-output_healthCheck = read_process("python ./health_check_repl.py")
