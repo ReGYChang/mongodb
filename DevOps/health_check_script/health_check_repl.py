c495c4d1517053c6a0139799d5b73ec3a0e3173e
Regy <regy@iro.local>
2021-12-28 14:50:29 +0800

    fix<healthCheck>: buildInfo extract version info

--- DevOps/health_check_script/health_check_repl.py
+++ DevOps/health_check_script/health_check_repl.py
@@ -159,7 +159,7 @@ for host in mongo_hosts:
 
     #log_path = re.findall(r"(path.+)",mongod_conf)[0].split(':')[1].strip()
     mongodb_port = re.findall(r"(port.+)",mongod_conf)[0].split(':')[1].strip()
-    mongod_version = read_process("mongo --quiet -port {} -u {} -p {} --eval 'db.serverBuildInfo()' > ./vars.js".format(mongodb_port,username,password))
+    mongod_version = read_process("mongo --quiet -port {} -u {} -p {} --eval 'db.serverBuildInfo().version' > ./vars.js".format(mongodb_port,username,password))
 
     if len(re.findall(r"tls:",mongod_conf)) > 0:
         isTls = True
